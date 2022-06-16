7d447c11361d93bbfc78b0cb3b89121ab16c6be6
Danila <103584554+Nepatrop@users.noreply.github.com>
2022-06-16 15:17:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -64,16 +64,16 @@ if choice == liveCamDetection:
         ret, frame = video_capture.read()
 
         # Resize frame of video to 1/4 size for faster face recognition processing
-        # small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        small_frame = cv2.resize(frame, (600, 600), fx=0.25, fy=0.25)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        # rgb_small_frame = frame[:, :, ::-1]
+        rgb_small_frame = small_frame[:, :, ::-1]
 
         # Only process every other frame of video to save time
         if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(frame)
-            face_encodings = face_recognition.face_encodings(frame, face_locations)
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
             face_names = []
             for face_encoding in face_encodings:
