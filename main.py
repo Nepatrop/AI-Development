15abdc92289cf799a98d7117ca37210034db8f30
Danila <103584554+Nepatrop@users.noreply.github.com>
2022-06-16 15:58:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ if choice == liveCamDetection:
         ret, frame = video_capture.read()
 
         # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (250, 250))
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
@@ -107,7 +107,7 @@ if choice == liveCamDetection:
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         # Display the resulting image
-        # cv2.imshow('Video', frame)
+        cv2.imshow('Video', frame)
         FRAME_WINDOW.image(frame)
 
         # Hit 'q' on the keyboard to quit!
