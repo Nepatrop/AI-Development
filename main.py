8b01f3438a6752d4cd3c43eaf7acea4c8659a666
Danila <103584554+Nepatrop@users.noreply.github.com>
2022-06-17 13:41:15 +0500

    Summary

--- main.py
+++ main.py
@@ -29,33 +29,72 @@ if choice == instruction:
 
 
 
-# DataSet Faces photos
-obabkov_F1 = face_recognition.load_image_file(
+obabkov_M1 = face_recognition.load_image_file(
     "DataSet Masks/9. Ilia Nikolaevich Obabkov 1.jpg")
+obabkov_M1_encoding = face_recognition.face_encodings(obabkov_M1)[0]
+
+obabkov_F1 = face_recognition.load_image_file(
+    "DataSet Faces/10. Ilia Nikolaevich Obabkov 2.png")
 obabkov_F1_encoding = face_recognition.face_encodings(obabkov_F1)[0]
 
-# Create arrays of known face encodings and their names
+shadrinD_M1 = face_recognition.load_image_file(
+    "DataSet Masks/12. Denis Borisovich Shadrin  1.jpg")
+shadrinD_M1_encoding = face_recognition.face_encodings(shadrinD_M1)[0]
+
+malevaniyA_F1 = face_recognition.load_image_file(
+    "DataSet Faces/16. Malevaniy Artem 2.jpeg")
+malevaniyA_F1_encoding = face_recognition.face_encodings(malevaniyA_F1)[0]
+
+malevaniyA_M1 = face_recognition.load_image_file(
+    "DataSet Masks/16. Malevaniy Artem 2.jpg")
+malevaniyA_M1_encoding = face_recognition.face_encodings(malevaniyA_M1)[0]
+
+akulovD_F1 = face_recognition.load_image_file(
+     "DataSet Faces/14. Akulov Danila 2.jpg")
+akulovD_F1_encoding = face_recognition.face_encodings(akulovD_F1)[0]
+
+akulovD_M1 = face_recognition.load_image_file(
+     "DataSet Masks/14. Akulov Danila 2.jpg")
+akulovD_M1_encoding = face_recognition.face_encodings(akulovD_M1)[0]
+
+sergeevaM_F1 = face_recognition.load_image_file(
+    "DataSet Faces/18. Sergeeva Maria 1.jpg")
+sergeevaM_F1_encoding = face_recognition.face_encodings(sergeevaM_F1)[0]
+
+sergeevaM_M1 = face_recognition.load_image_file(
+     "DataSet Masks/18. Sergeeva Maria 1.jpg")
+sergeevaM_M1_encoding = face_recognition.face_encodings(sergeevaM_M1)[0]
+
 known_face_encodings = \
     [
-        # Faces Encoding arr
-        obabkov_F1_encoding
+        obabkov_M1_encoding,
+        obabkov_F1_encoding,
+        shadrinD_M1_encoding,
+        malevaniyA_F1_encoding,
+        malevaniyA_M1_encoding,
+        akulovD_F1_encoding,
+        akulovD_M1_encoding,
+        sergeevaM_M1_encoding,
+        sergeevaM_F1_encoding
     ]
 
 known_face_names = \
     [
-        # Faces names Encoding arr
-        "Obabkov I. N."
+        "Obabkov I. N.",
+        "Obabkov I. N.",
+        "Shadrin D. B.",
+        "Malevaniy A. K.",
+        "Malevaniy A. K.",
+        "Akulov D. A.",
+        "Akulov D. A.",
+        "Sergeeva M.",
+        "Sergeeva M."
     ]
-
-
 
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
-
-
-
 FRAME_WINDOW = st.image([])
 
 
@@ -65,33 +104,25 @@ if choice == liveCamDetection:
     successCheck = True
     errorCheck = True
     while True:
-        # Grab a single frame of video
         ret, frame = video_capture.read()
         if not ret:
             continue
-        # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
 
-        # Only process every other frame of video to save time
         if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
             face_names = []
+
             for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
                 matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
                 name = "Unknown"
-
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
                 best_match_index = np.argmin(face_distances)
+
                 if matches[best_match_index]:
                     name = known_face_names[best_match_index]
-
                 face_names.append(name)
 
                 if name != "Unknown" and successCheck:
@@ -102,25 +133,19 @@ if choice == liveCamDetection:
                     errorCheck = False
         process_this_frame = not process_this_frame
 
-        # Display the results
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
             top *= 4
             right *= 4
             bottom *= 4
             left *= 4
 
-            # Draw a box around the face
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            # Draw a label with a name below the face
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-        # Display the resulting image
         FRAME_WINDOW.image(frame)
-    # Release handle to the webcam
     video_capture.release()
 
 
@@ -129,37 +154,27 @@ if choice == photoDetection:
 
     image_file = st.file_uploader(
         "Загрузите фотографию", type=['jpeg', 'png', 'jpg', 'webp'])
-
     if image_file:
         image = Image.open(image_file)
         successCheck = True
         errorCheck = True
         while True:
-            # Grab a single frame of video
             frame = np.array(image.convert('RGB'))
-            # Resize frame of video to 1/4 size for faster face recognition processing
             small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
             rgb_small_frame = small_frame[:, :, ::-1]
 
-            # Only process every other frame of video to save time
-            # if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
             face_names = []
+
             for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
                 matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
                 name = "Unknown"
-
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
                 best_match_index = np.argmin(face_distances)
+
                 if matches[best_match_index]:
                     name = known_face_names[best_match_index]
-
                 face_names.append(name)
 
                 if name != "Unknown" and successCheck:
@@ -169,23 +184,16 @@ if choice == photoDetection:
                     st.error("В доступе отказано")
                     errorCheck = False
 
-            #process_this_frame = not process_this_frame
-
-            # Display the results
             for (top, right, bottom, left), name in zip(face_locations, face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
                 top *= 4
                 right *= 4
                 bottom *= 4
                 left *= 4
 
-                # Draw a box around the face
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-                # Draw a label with a name below the face
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                 font = cv2.FONT_HERSHEY_DUPLEX
                 cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-            # Display the resulting image
             FRAME_WINDOW.image(frame)
